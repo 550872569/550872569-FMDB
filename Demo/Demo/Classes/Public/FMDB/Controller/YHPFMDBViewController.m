f037a95cdf7f7777ca0b9256c981b98696dc9785
YY <550872569@qq.com>
2016-08-22 13:04:00 +0800

    commit creat product and add gitignore (创建项目添加忽略文件)

---
+++ Demo/Demo/Classes/Public/FMDB/Controller/YHPFMDBViewController.m
@@ -0,0 +1,25 @@
+//
+//  YHPFMDBViewController.m
+//  Demo
+//
+//  Created by YY on 16/8/22.
+//  Copyright © 2016年 Yan. All rights reserved.
+//
+
+#import "YHPFMDBViewController.h"
+
+@interface YHPFMDBViewController ()
+
+@end
+
+@implementation YHPFMDBViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    NSLog(@"self%@",self);
+}
+@end
